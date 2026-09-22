d187094e51d318216acf6066d1aa82ff2bc3ff09
toxichome-whoami <toxic.home.whoami@hotmail.com>
2026-09-22 23:29:48 +0600

    update

--- app.cjs
+++
@@ -1,8 +0,0 @@
-// cPanel Passenger / LiteSpeed entry point
-// We use dynamic import() because require() fails on ESM modules with top-level await.
-import('tsx/esm/api').then(({ register }) => {
-    register();
-    return import('./src/server/index.ts');
-}).catch(err => {
-    console.error('Failed to start server:', err);
-});
